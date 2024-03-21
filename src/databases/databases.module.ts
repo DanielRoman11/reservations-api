db3a23bb2341dfbab470eebf3b0ead8ef7fab1cd
Daker <danielroman.05111@gmail.com>
2024-03-21 00:22:19 -0500

    feat(database)

--- src/databases/databases.module.ts
+++ src/databases/databases.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
-import { databaseProviders } from './database.providers';
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  providers: [...databaseProviders],
-  exports: [...databaseProviders]
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGO_URL || 'mongodb://localhost:27017/reservas',
+      logging: true,
+      synchronize: process.env.NODE_ENV === 'dev',
+      entities: [__dirname + "**/*.ts"]
+    }),
+  ],
 })
 export class DatabasesModule {}
