3d1e62ad30807034dfac17b444465a27aa096309
Daniel Roman2+git config --global user.name Daniel <danielroman.05111@gmail.com>
2024-05-17 03:26:41 +0000

    feat: Accomodation keys

--- src/accommodation/entities/accommodation.entity.ts
+++ src/accommodation/entities/accommodation.entity.ts
@@ -1,10 +1,49 @@
 import { Booking } from '../../booking/entities/booking.entity';
-import { Entity, OneToMany, PrimaryGeneratedColumn, Relation } from 'typeorm';
+import {
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  Relation,
+} from 'typeorm';
 
 @Entity()
 export class Accommodation {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
+  title: string;
+
+  @Column({ default: 1 })
+  rooms: number;
+
+  @Column({ default: 1 })
+  beds: number;
+
+  @Column({ default: 1 })
+  bathrooms: number;
+
+  @Column({ default: 1 })
+  price: number;
+
+  @Column({ default: true })
+  wifi: boolean;
+
+  @Column({ type: 'timestamp' })
+  arrivalDate: Date;
+
+  @Column({ type: 'timestamp' })
+  departureDate: Date;
+
+  @Column({ default: 1 })
+  guests: number;
+
+  @Column()
+  lat: string;
+
+  @Column()
+  lng: string;
 
   @OneToMany(() => Booking, (booking) => booking.accommodation)
   booking: Relation<Booking>;
