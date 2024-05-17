b092c14b4c8a3a430e7e1ca50dc033e9b3c3b8a0
Daniel Roman2+git config --global user.name Daniel <danielroman.05111@gmail.com>
2024-05-17 03:28:40 +0000

    feat: create user DTO

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsStrongPassword, Length } from 'class-validator';
+
 export class CreateUserDto {
-  email: string;
+  @Length(4, 20)
   username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword({ minNumbers: 1 })
+  @Length(4, 40)
   password: string;
 }
